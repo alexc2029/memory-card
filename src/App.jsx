763ffd356a4abcee21edc9d52f1dba09ab299ce3
alexc2029 <alex2akec@gmail.com>
2026-08-11 17:38:05 +0300

    feat: get heroes object in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import { getHeroes } from "./utils";
+
+const HERO_COUNT = 12;
+const url = "https://akabab.github.io/superhero-api/api";
+const heroes = await getHeroes(url, HERO_COUNT);
 
 function App() {
+	console.log(heroes);
 	return (
 		<>
 			<h1>Hello world!</h1>
