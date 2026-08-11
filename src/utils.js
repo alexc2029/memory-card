53d5cffa27a93fcbc99f3520679670066bc82581
alexc2029 <alex2akec@gmail.com>
2026-08-11 17:35:14 +0300

    feat: add utils for api calls

---
+++ src/utils.js
@@ -0,0 +1,37 @@
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomIds(count) {
+	const randomIds = [];
+	for (let i = 0; i < count; i++) {
+		randomIds.push(getRandomInt(1, 731));
+	}
+	console.log(randomIds);
+	return randomIds;
+}
+
+async function getHero(url, id) {
+	try {
+		const heroResponse = await fetch(`${url}/id/${id}.json`);
+		if (!heroResponse.ok) {
+			throw new Error(`Response status: ${heroResponse.status}`);
+		}
+		const heroResult = await heroResponse.json();
+		return heroResult;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function getHeroes(url, count) {
+	const id_array = getRandomIds(count);
+	const heroResults = await Promise.allSettled(
+		id_array.map((id) => getHero(url, id)),
+	);
+	return heroResults
+		.filter((result) => result.status === "fulfilled")
+		.map((result) => result.value);
+}
+
+export { getHeroes };
