52d2f575b225a3ed6a1ad8e748bc66c639ddd0b7
alexc2029 <alex2akec@gmail.com>
2026-08-12 19:32:20 +0300

    feat: add clicked state to cards

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,8 +1,13 @@
 import "./HeroCard.css";
+import { useState } from "react";
 
 function HeroCard({ name, imageUrl }) {
+	const [clicked, setClicked] = useState(false);
+	const handleClick = () => {
+		setClicked(true);
+	};
 	return (
-		<button type="button" className="hero-card">
+		<button type="button" className="hero-card" onClick={handleClick}>
 			<img src={imageUrl} alt="" />
 			<h2>{name}</h2>
 		</button>
