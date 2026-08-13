5437c6a352acf01f4af763e387c397bf84e7eea2
alexc2029 <alex2akec@gmail.com>
2026-08-13 10:04:39 +0300

    feat: define score state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "./App.css";
 import HeroGrid from "./components/HeroGrid.jsx";
 import Header from "./components/Header.jsx";
 import { getAllHeroes, getRandomHeroes, trimHeroes } from "./utils.js";
+import { useState } from "react";
 
 const HERO_COUNT = 12;
 const url = "https://akabab.github.io/superhero-api/api";
 const heroes = trimHeroes(getRandomHeroes(await getAllHeroes(url), HERO_COUNT));
 
 function App() {
+	const [score, setScore] = useState(0);
+	const increaseScore = () => {
+		setScore(score + 1);
+	};
+	const resetScore = () => {
+		setScore(0);
+	};
 	return (
 		<>
 			<Header />
