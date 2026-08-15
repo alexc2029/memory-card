436c36469f158644b81541d9d51ee91bf75e0570
alexc2029 <alex2akec@gmail.com>
2026-08-15 12:10:51 +0300

    refactor: fetch heroes in effect and remove redundant state

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import "./App.css";
 import HeroGrid from "./components/HeroGrid.jsx";
 import Header from "./components/Header.jsx";
 import { getAllHeroes, getRandomHeroes, trimHeroes, shuffle } from "./utils.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const HERO_COUNT = 12;
 const url = "https://akabab.github.io/superhero-api/api";
-const heroes = trimHeroes(getRandomHeroes(await getAllHeroes(url), HERO_COUNT));
 
 function App() {
+	const [heroes, setHeroes] = useState([]);
+	useEffect(() => {
+		async function fetchNewHeroes() {
+			setHeroes(
+				trimHeroes(
+					getRandomHeroes(await getAllHeroes(url), HERO_COUNT),
+				),
+			);
+		}
+		fetchNewHeroes();
+	}, []);
 	const [score, setScore] = useState(0);
 	const [bestScore, setBestScore] = useState(0);
 	const [resetKey, setResetKey] = useState(0);
-	const [shuffledHeroes, setShuffledHeroes] = useState(heroes);
 	const updateScore = (wasAlreadyClicked) => {
-		setShuffledHeroes(shuffle(shuffledHeroes));
+		setHeroes(shuffle(heroes));
 		if (wasAlreadyClicked) {
 			if (score > bestScore) setBestScore(score);
 			setScore(0);
@@ -27,7 +36,7 @@ function App() {
 		<>
 			<Header score={score} bestScore={bestScore} />
 			<HeroGrid
-				heroes={shuffledHeroes}
+				heroes={heroes}
 				resetKey={resetKey}
 				onClick={updateScore}
 			/>
